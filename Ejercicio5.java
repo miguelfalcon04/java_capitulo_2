87978dd4f6cba102ab9059192f882b9ccf1747c0
miguelfal <falconmiguel.2004@gmail.com>
2022-10-11 13:45:43 +0200

    commit

---
+++ Ejercicio5.java
@@ -0,0 +1,7 @@
+public class Ejercicio5 {
+    public static void main(String[] args){
+        int pesetas = 1000;
+        double euros = (pesetas * 0.006 );
+        System.out.print(pesetas + " pesetas son " + euros + " euros");
+    } 
+}
