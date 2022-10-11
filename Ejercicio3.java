84c8d88497c9f0edc59482b80f0bb6be1bb8e59f
miguelfal <falconmiguel.2004@gmail.com>
2022-10-11 13:25:54 +0200

    commit

---
+++ Ejercicio3.java
@@ -0,0 +1,10 @@
+public class Ejercicio3 {
+    public static void main(String[] args){
+        String nombre = "Miguel Falcón";
+        String direccion = "Calle Larios Malaga";
+        String telefono = "617 123 456";
+        System.out.println(nombre);
+        System.out.println(direccion);
+        System.out.println(telefono);
+    }
+}
