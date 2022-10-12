05d19a08e980ec28fe38e5ed7cbda2d945a81b5d
miguelfal <falconmiguel.2004@gmail.com>
2022-10-12 19:01:46 +0200

    commit

---
+++ Ejercicio7.java
@@ -0,0 +1,8 @@
+public class Ejercicio7 {
+    public static void main(String[] args){
+        char letraA = 'A';
+        char letraB = 'B';
+        String palabra = "palabra";
+        System.out.println(letraA + " " + letraB + " " + palabra);
+    }
+}
