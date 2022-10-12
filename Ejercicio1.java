e5779a64e11e562ae1f9f23254e49af4031c4f0f
miguelfal <falconmiguel.2004@gmail.com>
2022-10-12 18:51:20 +0200

    commit

--- Ejercicio1.java
+++ Ejercicio1.java
@@ -2,11 +2,11 @@ public class Ejercicio1{
     public static void main(String[] args){
         int x=144;
         int y= 999;
-        System.out.println(x);
-        System.out.println(y);
-        System.out.println(x+y);
-        System.out.println(x-y);
-        System.out.println(x/y);
-        System.out.println(x*y);
+        System.out.println("x = "+x);
+        System.out.println("y = "+y);
+        System.out.println("x + y = "+(x+y));
+        System.out.println("x - y = "+(x-y));
+        System.out.println("x / y = "+(x/y));
+        System.out.println("x * y = "+(x*y));
     }
 }
