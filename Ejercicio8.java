df1ee3c5b842cbb5c7a2bcb6c96a299dd89338fb
miguelfal <falconmiguel.2004@gmail.com>
2022-10-12 19:08:49 +0200

    commit

---
+++ Ejercicio8.java
@@ -0,0 +1,11 @@
+public class Ejercicio8 {
+    public static void main(String[] args){
+        char l1 = 'a';
+        char l2 = 'b';
+        char l3 = 'c';
+        char l4 = 'd';
+        char l5 = 'e';
+        String cadenacaracteres = "" + l1 + l2 + l3 + l4 + l5;
+        System.out.println(cadenacaracteres);
+    }
+}
