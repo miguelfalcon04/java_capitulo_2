f9b199a5c0316361dd1f99cfe2fc29a611ea3a80
miguelfal <falconmiguel.2004@gmail.com>
2022-10-13 20:25:57 +0200

    commit

--- Ejercicio6.java
+++ Ejercicio6.java
@@ -1,6 +1,8 @@
 public class Ejercicio6 {
     public static void main(String[] args){
-        
-
+        double baseImponible = 10.50;
+        System.out.printf("Base imponible %8.2f\n", baseImponible);
+        System.out.printf("IVA            %8.2f\n", (baseImponible * 0.21));
+        System.out.printf("Total          %8.2f\n", (baseImponible * 1.21));
     }
 }
